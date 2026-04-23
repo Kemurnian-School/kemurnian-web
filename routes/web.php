98e0c401701ffefbbf1c4ddf0f656580b40965a5
MystiaFin <cytrsx01@gmail.com>
2026-04-23 14:04:08 +0700

    feat: add admin hero routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\HeroController;
 
 Route::prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index']);
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/hero', [HeroController::class, 'index']);
+    Route::get('/hero/create', [HeroController::class, 'create']);
+    Route::post('/hero', [HeroController::class, 'store']);
+    Route::delete('/hero/{id}', [HeroController::class, 'destroy']);
+    Route::post('/hero/reorder', [HeroController::class, 'reorder']);
 });
