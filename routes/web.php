7bfff324ec0323a99cdc037834dde659e08e29e1
MystiaFin <cytrsx01@gmail.com>
2026-04-23 14:33:29 +0700

    fix: merge admin route groups and add named routes

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,19 @@ Route::prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index']);
 });
 
-Route::prefix('admin')->group(function () {
-    Route::get('/hero', [HeroController::class, 'index']);
-    Route::get('/hero/create', [HeroController::class, 'create']);
-    Route::post('/hero', [HeroController::class, 'store']);
-    Route::delete('/hero/{id}', [HeroController::class, 'destroy']);
-    Route::post('/hero/reorder', [HeroController::class, 'reorder']);
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/hero', [HeroController::class, 'index'])->name('hero');
+    Route::get('/hero/create', [HeroController::class, 'create'])->name('hero.create');
+    Route::post('/hero', [HeroController::class, 'store'])->name('hero.store');
+    Route::delete('/hero/{id}', [HeroController::class, 'destroy'])->name('hero.destroy');
+    Route::post('/hero/reorder', [HeroController::class, 'reorder'])->name('hero.reorder');
+});
+
+Route::get('/debug-hero', function () {
+    $hero = \App\Models\Hero::first();
+    return [
+        'raw' => $hero->getRawOriginal('desktop_image'),
+        'accessor' => $hero->desktop_image,
+    ];
 });
